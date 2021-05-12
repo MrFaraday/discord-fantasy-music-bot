a76eb5690e5d3c1160ec412110c8dff88df1ad5d
MrFaraday <faradayby@gmail.com>
2021-05-12 15:46:38 +0300

    Update packages

--- src/api/youtube-api.ts
+++ src/api/youtube-api.ts
@@ -1,7 +1,7 @@
 import { URL } from 'url'
 import axios from 'axios'
 import { MAX_QUEUE_LENGTH, YOUTUBE_API_KEY } from '../config'
-import ytdl from 'discord-ytdl-core'
+import ytdl from 'ytdl-core-discord'
 
 if (!YOUTUBE_API_KEY) {
     throw new Error('Environment variable YOUTUBE_API_KEY not found')
@@ -91,14 +91,7 @@ class YoutubeApi {
 
         return {
             title: await this.getVideoTitle(videoId),
-            getStream: () =>
-                Promise.resolve(
-                    ytdl(url, {
-                        filter: 'audioonly',
-                        opusEncoded: true,
-                        encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-                    })
-                ),
+            getStream: () => ytdl(url),
             meta: [['url', url]]
         }
     }
@@ -114,14 +107,7 @@ class YoutubeApi {
 
             return {
                 title,
-                getStream: () =>
-                    Promise.resolve(
-                        ytdl(url, {
-                            filter: 'audioonly',
-                            opusEncoded: true,
-                            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-                        })
-                    ),
+                getStream: () => ytdl(url),
                 meta: [['url', url]]
             }
         })
