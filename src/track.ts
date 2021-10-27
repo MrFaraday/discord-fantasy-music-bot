83a7c357692caf00814788fe4e68bb981fb6787b
MrFaraday <faradayby@gmail.com>
2021-10-28 01:11:42 +0300

    refactor: remove extra libs

--- src/track.ts
+++ src/track.ts
@@ -1,8 +1,6 @@
 import { AudioResource, createAudioResource, demuxProbe } from '@discordjs/voice'
 import { MessageEmbed, TextBasedChannels } from 'discord.js'
 import { EMBED_COLOR } from './config'
-// import { raw as ytdl } from 'youtube-dl-exec'
-// import ytdl from 'ytdl-core-discord'
 import { stream as playStream } from 'play-dl'
 
 interface TrackData {
@@ -37,71 +35,16 @@ export class Track implements TrackData {
         try {
             await this.textChannel.send({ embeds: [embed] })
         } catch (error) {
-            console.warn(error)
             // unable to send embed
         }
     }
 
     public async createAudioResource (): Promise<AudioResource<Track>> {
-        // play-dl
         const stream = await playStream(this.url)
         return createAudioResource(stream.stream, {
             inputType: stream.type,
-            inlineVolume: true
+            inlineVolume: true,
+            metadata: this
         })
-        //
-        // ytld-core
-        //
-        // try {
-        //     const stream: Stream = await ytdl(this.url)
-        //     // const probe = await demuxProbe(stream)
-        //     play
-        //     return createAudioResource(stream, {
-        //         metadata: this,
-        //         inputType: stream.type
-        //     })
-        // } catch (error) {
-        //     console.error(error)
-        //     throw error
-        // }
-        //
-        // exec
-        //
-        // const process = ytdl(
-        //     this.url,
-        //     {
-        //         o: '-',
-        //         q: '',
-        //         f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
-        //         r: '100K'
-        //     },
-        //     { stdio: ['ignore', 'pipe', 'ignore'] }
-        // )
-        // if (!process.stdout) {
-        //     throw new Error('No stdout')
-        // }
-        // const stream = process.stdout
-        // return new Promise((resolve, reject) => {
-        //     const onError = (error: Error) => {
-        //         if (!process.killed) process.kill()
-        //         stream.resume()
-        //         reject(error)
-        //     }
-        //     process
-        //         .once('spawn', () => {
-        //             demuxProbe(stream)
-        //                 .then((probe) =>
-        //                     resolve(
-        //                         createAudioResource(probe.stream, {
-        //                             metadata: this,
-        //                             inputType: probe.type,
-        //                             inlineVolume: true
-        //                         })
-        //                     )
-        //                 )
-        //                 .catch(onError)
-        //         })
-        //         .catch(onError)
-        // })
     }
 }
